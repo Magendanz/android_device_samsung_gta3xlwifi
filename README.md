3880c3bc3c5a03aad1ba0f7c604aa828d1cba6bf
Chad L Magendanz <chad@magendanz.com>
2019-05-05 12:25:23 -0700

    Initial commit based on android_device_samsung_beyond1lte

---
+++ README.md
@@ -0,0 +1,10 @@
+## Recovery Device Tree for the 2019 Samsung Galaxy Tab A [SM-T510/T515]
+
+## How-to compile it:
+
+To build:
+
+```sh
+. build/envsetup.sh
+lunch omni_gta3xlwifi-eng
+make recoveryimage
---
+++ omni_gta3xlwifi.mk
@@ -0,0 +1,15 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from those products. Most specific first.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_gta3xlwifi
+PRODUCT_DEVICE := gta3xlwifi
+PRODUCT_MODEL := SM-T510
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
