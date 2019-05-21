7e2483f88f94e4ac609d80806b2b925c509f523a
Chad Magendanz <chad@magendanz.com>
2019-05-21 01:27:12 -0700

    Add custom kernel; Build arm64 recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,40 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_FOLDER := device/samsung/gta3xlwifi
+
 # Architecture
-TARGET_ARCH := arm
+TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_USES_64_BIT_BINDER := true
 
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-
 ALLOW_MISSING_DEPENDENCIES=true
 
 # Bootloader
@@ -24,12 +48,18 @@ TARGET_NO_RADIOIMAGE := true
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_PREBUILT_KERNEL := device/samsung/gta3xlwifi/prebuilt/kernel
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPSA25A001RU
+TARGET_PREBUILT_KERNEL := $(DEVICE_FOLDER)/prebuilt/kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_FOLDER)/prebuilt/recovery_dtbo
 
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_BOARD_PLATFORM_GPU := mali-g71
+USE_OPENGL_RENDERER := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x02000000
@@ -38,51 +68,59 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xB8000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0xFFFFFFFF
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 0x06800000
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 0x18000000
-BOARD_PRODUCTIMAGE_PARTITION_SIZE := 0x2F000000
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+#BOARD_PRODUCTIMAGE_PARTITION_SIZE := 0x2F000000
 BOARD_DTBOIMAGE_PARTITION_SIZE := 0x00800000
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Filesystem
-BOARD_HAS_LARGE_FILESYSTEM := true
+# Workaround for error copying vendor files to recovery ramdisk
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_FOLDER)/recovery/root/fstab.exynos7885
+TARGET_RECOVERY_INITRC := $(DEVICE_FOLDER)/recovery/root/init.rc
+TARGET_OTA_ASSERT_DEVICE := gta3xlwifi
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Android Verified Boot
-BOARD_AVB_ENABLE := true
-#BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS := 
-ifeq ($(BOARD_AVB_ENABLE), true)
-   BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-   BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
-   BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
-   BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-endif
+BOARD_AVB_ENABLE := false
+BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
 # Crypto
-#TARGET_HW_DISK_ENCRYPTION := true
 TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO_SAMSUNG := true
 TW_INCLUDE_FBE := true
+#TARGET_HW_DISK_ENCRYPTION := true
+
+# Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TWRP_EVENT_LOGGING := false
 
 # TWRP specific build flags
 RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
+TW_HAS_MTP := true
 RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 25500
-TW_DEFAULT_BRIGHTNESS := 15300
-TW_Y_OFFSET := 142
-TW_H_OFFSET := -142
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 150
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_NTFS_3G := true
 TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
 TW_EXTRA_LANGUAGES := true
 TW_USE_NEW_MINADBD := true
 TW_USE_TOOLBOX := true
-TW_EXCLUDE_TWRPAPP := true
 TW_NO_LEGACY_PROPS := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-TW_DEVICE_VERSION := 1_ianmacd
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_PREBUILT_DTBOIMAGE := device/samsung/gta3xlwifi/prebuilt/recovery_dtbo
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_DEVICE_VERSION := 0
