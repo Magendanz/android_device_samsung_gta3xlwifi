7e1e6bd24394d3c4b54e0a98c0e0e16b223c78b0
Chad Magendanz <chad@magendanz.com>
2019-05-29 08:33:43 -0700

    First XDA public release; arm w/ no MTP or FBE

--- omni_gta3xlwifi.mk
+++ omni_gta3xlwifi.mk
@@ -1,11 +1,11 @@
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_gta3xlwifi
